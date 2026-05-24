40dd8878f2ee37632dd66adb018fa93982773f71
José Ignacio Falla García <joseignacio.falla67@gmail.com>
2026-05-24 23:30:30 +0200

    Update InteractorInterface.h

---
+++ Interface/InteractorInterface.h
@@ -0,0 +1,14 @@
+#include "Interfaces/InteractorInterface.h"
+
+/*
+/////////////////////////////////////
+INTERACTOR INTERFACE
+
+Intentionally minimal.
+
+This translation unit exists to:
+- satisfy UE build systems
+- avoid linker oddities in UE4.26
+- provide future expansion point
+/////////////////////////////////////
+*/
