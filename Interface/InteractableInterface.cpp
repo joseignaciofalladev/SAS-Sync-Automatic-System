a5b95a514038647ded564f5fd5a67680877bdb8e
José Ignacio Falla García <joseignacio.falla67@gmail.com>
2026-05-24 23:30:54 +0200

    Create InteractableInterface.cpp

---
+++ Interface/InteractableInterface.cpp
@@ -0,0 +1,14 @@
+#include "Interfaces/InteractableInterface.h"
+
+/*
+/////////////////////////////////////
+INTERACTABLE INTERFACE
+
+Intentionally minimal.
+
+This translation unit exists to:
+- satisfy UE build systems
+- avoid linker oddities in UE4.26
+- provide future expansion point
+/////////////////////////////////////
+*/
