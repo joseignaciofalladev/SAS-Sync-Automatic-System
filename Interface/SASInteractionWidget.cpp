28a42d7054d363e0fe16ae0576e0b7eef039a67a
José Ignacio Falla García <joseignacio.falla67@gmail.com>
2026-05-26 11:51:06 +0200

    Create SASInteractionWidget.cpp

---
+++ Interface/SASInteractionWidget.cpp
@@ -0,0 +1,72 @@
+#include "UI/SASInteractionWidget.h"
+#include "Components/Widget.h"
+
+USASInteractionWidget::USASInteractionWidget(
+	const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer){
+	InteractionProgress = 0.f;
+	bVisible = false;
+}
+
+void USASInteractionWidget::NativeConstruct(){
+	Super::NativeConstruct();
+	SetVisibility(ESlateVisibility::Collapsed);
+}
+
+void USASInteractionWidget::NativeDestruct(){
+	TargetActor = nullptr;
+	Super::NativeDestruct();
+}
+
+/////////////////////////////////////
+// TARGET
+/////////////////////////////////////
+
+void USASInteractionWidget::SetTargetActor(
+	AActor* NewTargetActor){
+	if (TargetActor.Get() == NewTargetActor){
+		return;
+	}
+
+	TargetActor = NewTargetActor;
+	BP_OnTargetChanged(NewTargetActor);
+}
+
+void USASInteractionWidget::ClearTargetActor(){
+	if (!TargetActor.IsValid()){return;}
+
+	TargetActor = nullptr;
+	InteractionProgress = 0.f;
+	BP_OnTargetCleared();
+}
+
+/////////////////////////////////////
+// VISIBILITY
+/////////////////////////////////////
+
+void USASInteractionWidget::ShowWidget(){
+	if (bVisible){return;}
+	bVisible = true;
+	SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+	BP_OnWidgetShown();
+}
+
+void USASInteractionWidget::HideWidget(){
+	if (!bVisible){return;}
+	bVisible = false;
+	SetVisibility(ESlateVisibility::Collapsed);
+	InteractionProgress = 0.f;
+	BP_OnWidgetHidden();
+}
+
+void USASInteractionWidget::SetInteractionProgress(
+	float NewProgress){
+	const float ClampedProgress = FMath::Clamp(NewProgress,0.f,1.f);
+
+	if (FMath::IsNearlyEqual(InteractionProgress,ClampedProgress)){
+		return;
+	}
+
+	InteractionProgress = ClampedProgress;
+	BP_OnProgressUpdated(InteractionProgress);
+}
